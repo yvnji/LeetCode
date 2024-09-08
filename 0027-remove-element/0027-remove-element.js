be900a9b8b5cf3c512095bf61bc6dc1ae67999ed
yvnji <yvnji25@gmail.com>
2024-09-08 18:20:38 +0900

    Time: 49 ms (78.61%), Space: 49.4 MB (10.37%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    k = 0;
+    
+    for (i = 0; i <nums.length; i++) {
+        if(nums[i] !== val) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+    
+    return k;
+};
